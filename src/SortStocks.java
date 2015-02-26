e1231b378ac78d356059cd40171fb5b9a297eb30
JoePeacock <jpeacockinc@gmail.com>
2015-02-25 23:10:52 -0500

    Added in sorting functionality, testing now

--- src/SortStocks.java
+++ src/SortStocks.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.TreeMap;
+
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -11,13 +14,13 @@ public class SortStocks {
 	 * 
 	 * @author joepeacock
 	 */
-	public static class Map extends Mapper<Object, Text, DoubleWritable, Text>{
+	public static class Map extends Mapper<Object, Text, Text, Text>{
 		
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			
 			// Map our volatility's to the key, which is sorted into the reducer, and the stock name as the value.
 			String[] columns = value.toString().split("\t");
-			context.write(new DoubleWritable(Double.parseDouble(columns[1])), new Text(columns[0]));
+			context.write(new Text("Stock"), new Text(columns[0] + "," + columns[1]));
 		}
 	}
 	
@@ -26,11 +29,37 @@ public class SortStocks {
 	 * @author joepeacock
 	 *
 	 */
-	public static class Reduce extends Reducer<DoubleWritable, Text, Text, DoubleWritable> {
+	public static class Reduce extends Reducer<Text, Text, Text, DoubleWritable> {
 
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
+			TreeMap<Double, String> sortedStocks = new TreeMap<Double, String>();
+			
+			for (Text val: values) {
+				String[] stock = val.toString().split(",");
+				sortedStocks.put(Double.parseDouble(stock[0]), stock[1]);
+			}
+			
+			context.write(new Text("\nTop 10 Volatility\n"), new DoubleWritable());
 
+			for (int i =0; i<10; i++) {
+				double volatility = sortedStocks.firstEntry().getKey();
+				String stockName = sortedStocks.firstEntry().getValue();
+
+				context.write(new Text(stockName), new DoubleWritable(volatility));
+				
+				sortedStocks.remove(sortedStocks.firstEntry());
+			}
+			
+			context.write(new Text("\nBottom 10 Volatility\n"), new DoubleWritable());
+			for(int i =0; i<10; i++) {
+				double volatility = sortedStocks.lastEntry().getKey();
+				String stockName = sortedStocks.lastEntry().getValue();
+
+				context.write(new Text(stockName), new DoubleWritable(volatility));
+				
+				sortedStocks.remove(sortedStocks.lastEntry());
+			}
 		}
 	}
 }
